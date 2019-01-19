af8fea8a7c961b8e573ad2c8caf256bbc8d3362e
Vova Pankiv <vovapankiw@gmail.com>
2019-01-19 08:18:39 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import VueResource from 'vue-resource'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const router =  new VueRouter({
+  routes,
+  mode:'history',
+});
+
+Vue.use(VueResource);
+Vue.http.options.root ='https://madagascar-test.firebaseio.com'
+
 new Vue({
   render: h => h(App),
+  router,
+  store,
 }).$mount('#app')
